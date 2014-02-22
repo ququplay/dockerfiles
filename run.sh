2938f02ac610528df57233379b4b5b27ed7fc11e
Michal Kuklis <michal.kuklis@gmail.com>
2014-02-21 21:13:40 -0700

    link mongodb and redis to nodejs container

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 
 sudo docker run -name mongodb -p 127.0.0.1:27017:27017 -v /mnt/db:/data/db -d ququplay/mongodb
 sudo docker run -name redis -p 127.0.0.1:6379:6379 -v /mnt/redis:/redis-data -d ququplay/redis-server
+sudo docker run -name nodejs -link mongodb:mongodb -link redis:redis -d ququplay/nodejs
