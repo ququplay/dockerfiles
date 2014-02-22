0c14dda0921bbb54584c787cd05d7580fb7c80a3
Michal Kuklis <michal.kuklis@gmail.com>
2014-02-21 21:08:18 -0700

    added container names

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker run -p 127.0.0.1:27017:27017 -v /mnt/db:/data/db -d ququplay/mongodb
-sudo docker run -p 127.0.0.1:6379:6379 -v /mnt/redis:/redis-data -d ququplay/redis-server
+sudo docker run -name mongodb -p 127.0.0.1:27017:27017 -v /mnt/db:/data/db -d ququplay/mongodb
+sudo docker run -name redis -p 127.0.0.1:6379:6379 -v /mnt/redis:/redis-data -d ququplay/redis-server
