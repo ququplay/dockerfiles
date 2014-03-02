7f541e6d35ba1b3b09cf4636ce5a4aed852cd609
Michal Kuklis <michal.kuklis@gmail.com>
2014-03-01 19:05:59 -0500

    link to redis and mongo from node

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 sudo docker run -name mongodb -p 127.0.0.1:27017:27017 -v /mnt/db:/data/db -d ququplay/mongodb
 sudo docker run -name redis -p 127.0.0.1:6379:6379 -v /mnt/redis:/redis-data -d ququplay/redis-server
-# sudo docker run -v ~/www:/www ququplay/nodejs npm install /www/
+sudo docker run -name nodejs -p 0.0.0.0:8080:8080 -link redis:redis -link mongodb:mongodb -v ~/www:/www ququplay/nodejs node /www/
